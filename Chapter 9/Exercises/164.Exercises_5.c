426d053f937af14151bcde13e5daaa5d1a6a5e9c
aws almahasneh <awsalmahasneh@gmail.com>
2024-06-04 22:44:17 +0300

    Upload first 5 CH9 Exercises

---
+++ Chapter 9/Exercises/164.Exercises_5.c
@@ -0,0 +1,28 @@
+/**********************************************************************************
+* Name: 164.Exercises_5.c				    			  					  	  *
+* Purpose: Solve Exercise 														  *
+* Author: Aws Almahasneh 				  										  *
+**********************************************************************************/
+
+#include <stdio.h>
+
+int num_digits(int n);
+
+int main(void)
+{
+	printf("%d\n",num_digits(1111111));
+
+	return 0;
+}
+
+int num_digits(int n)
+{
+	int count=0;
+
+	while(n>0)
+	{
+		n/=10;
+		count++;
+	}
+	return count;
+}
